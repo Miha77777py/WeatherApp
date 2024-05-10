23778160689b82a849e7312bfe97c55a3e0ebfda
Miha77777 <lv11miha@gmail.com>
2024-05-10 20:26:32 +0300

    fixed setupper

---
+++ forge.config.js
@@ -0,0 +1,51 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: "./FE/img/icon.ico",
+    win32metadata:{
+      ProductName: "Weather",
+      CompanyName: "Miha77777py"
+    }
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {authors: "Miha77777",
+        description: "Nice program!!!"
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const {app, BrowserWindow} = require("electron");
 const path = require("path")
 
+if(require("electron-squirrel-startup")) app.quit();
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 700,
